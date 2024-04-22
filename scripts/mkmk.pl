fdb61583e8b6901f126041f51cf6d07c79bb307e
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-04-22 18:43:00 +0200

    update build module

--- scripts/mkmk.pl
+++ scripts/mkmk.pl
@@ -115,7 +115,8 @@ foreach $fn (@f) {
     foreach (keys %d) { $d{$_} =~ s/\$\{ver_\}/$ver_/ge; }
     ## replace ${ver2} with the 2 segments version
     if ($ver =~ /^(\d+\.\d+)\.\d+$/) {
-        foreach (keys %d) { $d{$_} =~ s/\$\{ver2\}/$1/ge; }
+        my $ver2 = $1
+        foreach (keys %d) { $d{$_} =~ s/\$\{ver2\}/$ver2/ge; }
     }
     my $src = $d{"source.url"};
     my $pkg = $d{"package"};
